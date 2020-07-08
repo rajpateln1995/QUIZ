0e46bafd439f14079dbd790106a6eadc1922b486
Raj Ghamsani <54090909+rajpateln1995@users.noreply.github.com>
2020-07-08 18:10:17 +0530

    S

--- django_school/django_school/settings.py
+++ django_school/django_school/settings.py
@@ -107,9 +107,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
