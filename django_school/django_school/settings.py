6f4611c547ebb6f72f16dcf43180f1a6eecae9bb
Raj Ghamsani <54090909+rajpateln1995@users.noreply.github.com>
2020-07-08 19:28:11 +0530

    as

--- django_school/django_school/settings.py
+++ django_school/django_school/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 import dj_database_url
 from django.contrib.messages import constants as messages
 
-
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
